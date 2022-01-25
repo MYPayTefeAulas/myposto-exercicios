e62dfd02aa5f28b4016f1c682d4216c817a53763
Raiane <rfm.fis17@uea.edu.br>
2022-01-25 16:07:23 -0400

    25/01/2022

---
+++ MyPayTemplate/app/src/main/java/br/uea/transirie/mypay/mypaytemplate/viewmodel/AbastecerGasolinaViewModel.kt
@@ -0,0 +1,8 @@
+package br.uea.transirie.mypay.mypaytemplate.viewmodel
+
+import br.uea.transirie.mypay.mypaytemplate.repository.room.AppDatabase
+
+class AbastecerGasolinaViewModel (appDatabase: AppDatabase) {
+
+    private val
+}
